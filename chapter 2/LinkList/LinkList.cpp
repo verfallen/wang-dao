65fac1745280e02369937109aee1786afbe06de8
王晓丽 <wangxiaoli@troila.com>
2020-11-03 13:32:42 +0800

    尾插法建立单链表

--- chapter 2/LinkList/LinkList.cpp
+++ chapter 2/LinkList/LinkList.cpp
@@ -32,9 +32,33 @@ LinkList List_HeadInsert(LinkList &L)
   return L;
 }
 
+//尾插法建立单链表
+LinkList List_TailInsert(LinkList &L)
+{
+  LNode *s, *r = L; //r 表示尾指针
+  int x;
+
+  L = (LinkList)malloc(sizeof(LNode));
+  L->next = NULL;
+
+  scanf("%d", &x);
+
+  while (x != 9999)
+  {
+    s = (LNode *)malloc(sizeof(LNode));
+    s->data = x;
+    r->next = s;
+    r = s;
+    scanf("%d", &x);
+  }
+  r->next = NULL;
+  return L;
+}
+
 int main(int argc, const char **argv)
 {
   LinkList L;
-  List_HeadInsert(L);
+  // List_HeadInsert(L);
+  List_TailInsert(L);
   return 0;
 }
