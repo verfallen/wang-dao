a1cab43fdf161a91a03f1a3253c002b8000e2b5f
王晓丽 <wangxiaoli@troila.com>
2020-11-14 22:27:04 +0800

    静态分配的顺序表

---
+++ chapter 2/SqlList/StaticSqlList.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#define MaxSize 10
+
+typedef struct
+{
+    int data[MaxSize];
+    int length;
+} SqList;
+
+void InitList(SqList &L)
+{
+    // for (int i = 0; i < MaxSize; i++)
+    // L.data[i] = 0;
+    L.length = 0;
+}
+
+void PrintList(SqList L)
+{
+    for (int i = 0; i < L.length; i++)
+        printf("%d", L.data[i]);
+    printf("\n");
+}
+
+int main(int argc, char const *argv[])
+{
+    SqList L;
+    InitList(L);
+    for (int i = 0; i < MaxSize; i++)
+        printf("data[%d]=%d\n", i, L.data[i]);
+    return 0;
+}
