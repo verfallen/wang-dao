a728c9883ab0860c5df9c2099d6d34c1bfb8a0d6
王晓丽 <wangxiaoli@troila.com>
2020-11-14 22:54:37 +0800

    更改tab 空格为2

--- chapter 2/SqlList/StaticSqlList.cpp
+++ chapter 2/SqlList/StaticSqlList.cpp
@@ -4,43 +4,53 @@
 
 typedef struct
 {
-    int data[MaxSize];
-    int length;
+  int data[MaxSize];
+  int length;
 } SqList;
 
 void InitList(SqList &L)
 {
-    // for (int i = 0; i < MaxSize; i++)
-    // L.data[i] = 0;
-    L.length = 0;
+  // for (int i = 0; i < MaxSize; i++)
+  // L.data[i] = 0;
+  L.length = 0;
 }
 
 void PrintList(SqList L)
 {
-    for (int i = 0; i < L.length; i++)
-        printf("%d", L.data[i]);
-    printf("\n");
+  for (int i = 0; i < L.length; i++)
+    printf("%d", L.data[i]);
+  printf("\n");
 }
 
 //插入到位序i
 bool ListInsert(SqList &L, int i, int e)
 {
-    if (i < 1 || i > L.length + 1)
-        return false;
-    if (L.length >= MaxSize)
-        return false;
-    for (int j = L.length - 1; j >= i; j)
-        L.data[j] = L.data[j - 1];
-    L.data[i - 1] = e;
-    L.length++;
-    return true;
+  if (i < 1 || i > L.length + 1)
+    return false;
+  if (L.length >= MaxSize)
+    return false;
+  for (int j = L.length - 1; j >= i; j)
+    L.data[j] = L.data[j - 1];
+  L.data[i - 1] = e;
+  L.length++;
+  return true;
 }
 
 int main(int argc, char const *argv[])
 {
-    SqList L;
-    InitList(L);
-    for (int i = 0; i < MaxSize; i++)
-        printf("data[%d]=%d\n", i, L.data[i]);
-    return 0;
+  SqList L;
+  InitList(L);
+  ListInsert(L, 1, 1);
+  ListInsert(L, 1, 2);
+  ListInsert(L, 1, 3);
+
+  PrintList(L);
+
+  int e = -1;
+
+  // if (ListDelete(L, 3, e))
+  // {
+  //   printf("删除第3个元素，为%d", e);
+  // }
+  return 0;
 }
