95386ebf4e5684afc9ed2ab4f9071442ba0a8189
王晓丽 <wangxiaoli@troila.com>
2020-11-15 13:09:46 +0800

    逆置两个有序表

--- chapter 2/SqlList/SqlList.cpp
+++ chapter 2/SqlList/SqlList.cpp
@@ -235,7 +235,33 @@ bool DelSame(SqlList &L)
   return true;
 }
 
-//7 合并有序顺序表
+//8 将AB两个顺序表逆置为 BA
+/**
+ * 1. 将AB 原地逆置为（BA）-1
+ * 2. 分别将前n个和后m个逆置
+ */
+
+typedef int DataType;
+void Reverse(DataType A[], int left, int right, int arraySize)
+{
+  if (left >= right || right >= arraySize)
+    return;
+  int mid = (left + right) / 2;
+
+  for (int i = 0; i < mid; i++)
+  {
+    DataType tmp = A[left + i];
+    A[left + i] = A[right - i];
+    A[right - i] = tmp;
+  }
+}
+
+void Exchange(DataType A[], int m, int n, int arraySize)
+{
+  Reverse(A, 0, m + n - 1, arraySize);
+  Reverse(A, 0, n - 1, arraySize);
+  Reverse(A, n, m + n - 1, arraySize);
+}
 
 void testDelMin(SqlList &L)
 {
