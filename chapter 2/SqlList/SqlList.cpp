82d1a58c52fea9a48b91670f3e51808c4342c8de
王晓丽 <wangxiaoli@troila.com>
2020-11-15 13:41:25 +0800

    10 循环左移p个位置

--- chapter 2/SqlList/SqlList.cpp
+++ chapter 2/SqlList/SqlList.cpp
@@ -301,6 +301,29 @@ void SearxhExchangeInsert(ElementType A[], int n, ElementType x)
   }
 }
 
+//10：循环左移p个位置
+void swap(int &a, int &b)
+{
+  int tmp;
+  tmp = a;
+  a = b;
+  b = tmp;
+}
+void Reverse(int R[], int from, int to)
+{
+  int i, tmp;
+  for (i = 0; i < (to - from + 1) / 2; i++)
+    swap(R[from + i], R[to - i]);
+}
+void Converse(int R[], int n, int p)
+{
+  Reverse(R, 0, p - 1);
+  Reverse(R, p, n - 1);
+  Reverse(R, 0, n - 1);
+}
+
+
+
 void testDelMin(SqlList &L)
 {
 
