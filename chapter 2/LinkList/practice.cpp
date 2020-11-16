3556d77835329f02b139934ac40988e2fea9cde4
王晓丽 <wangxiaoli@troila.com>
2020-11-16 10:28:48 +0800

    反向输出带头结点的单链表

--- chapter 2/LinkList/practice.cpp
+++ chapter 2/LinkList/practice.cpp
@@ -26,15 +26,24 @@ void delete_x(LinkList &L, int x)
   }
 }
 
+//3. 反向输出带头结点的单链表
+void R_Print(LinkList L)
+{
+  if (L->next != NULL)
+    R_Print(L->next);
+  printf("%d", L->data);
+}
+
 int main(int argc, const char **argv)
 {
   LinkList L;
   InitList(L);
-  List_HeadInsert(L);
+  List_TailInsert(L);
   PrintList(L);
 
-  std::cout << "delete value 2" << std::endl;
-  delete_x(L, 2);
-  PrintList(L);
+  // std::cout << "delete value 2" << std::endl;
+  // delete_x(L, 2);
+  // PrintList(L);
+  R_Print(L);
   return 0;
 }
