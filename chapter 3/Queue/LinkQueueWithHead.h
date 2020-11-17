d7ec10991dd1d8a2805e92f9c29113d04074f4ec
王晓丽 <wangxiaoli@troila.com>
2020-11-17 15:26:25 +0800

    带头结点的链队

---
+++ chapter 3/Queue/LinkQueueWithHead.h
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+using namespace std;
+/**
+ * 带头结点的链队
+ * rear 指向最后一个节点
+ */
+
+typedef int ElemType;
+typedef struct LinkNode
+{
+  ElemType data;
+  struct LinkNode *next;
+} LinkNode;
+
+typedef struct
+{
+  LinkNode *front, *rear;
+} LinkQueue;
+
+void InitQueque(LinkQueue &Q);
+bool Empty(LinkQueue Q);
+bool EnQueue(LinkQueue &Q, ElemType x);
+bool DeQueue(LinkQueue &Q, ElemType &x);
+
+void InitQueque(LinkQueue &Q)
+{
+  Q.front = Q.rear = (LinkNode *)malloc(sizeof(LinkNode));
+  Q.front->next = NULL;
+}
+
+bool Empty(LinkQueue Q)
+{
+  return Q.front == Q.rear;
+}
+
+bool EnQueue(LinkQueue &Q, ElemType x)
+{
+  LinkNode *s = (LinkNode *)malloc(sizeof(LinkNode));
+  s->data = x;
+  s->next = NULL;
+
+  Q.rear->next = s;
+  Q.rear = s;
+}
+
+bool DeQueue(LinkQueue &Q, ElemType &x)
+{
+  if (Empty(Q))
+    return false;
+  LinkNode *p = Q.front->next;
+  x = p->data;
+  Q.front->next = p->next;
+  if (Q.rear == p)
+    Q.rear = Q.front;
+  free(p);
+  return true;
+}
