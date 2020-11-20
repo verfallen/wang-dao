48892a30549e0fbed3c63b6b999730eb5a86c0df
王晓丽 <wangxiaoli@troila.com>
2020-11-20 12:14:55 +0800

    树的顺序存储

---
+++ chapter5/Tree.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define MaxSize 100
+
+typedef int ElmeType;
+
+//数组存储下标从1开始
+struct TreeNode
+{
+  ElmeType value;
+  bool isEmpty;
+};
+
+int main(int argc, const char **argv)
+{
+  TreeNode t[MaxSize];
+  return 0;
+}
