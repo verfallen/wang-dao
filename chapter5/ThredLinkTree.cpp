c4fb23fc319049adef6de8745b5a56e74d6484be
王晓丽 <wangxiaoli@troila.com>
2020-11-21 12:54:06 +0800

    中序线索化（王道教材版）

--- chapter5/ThredLinkTree.cpp
+++ chapter5/ThredLinkTree.cpp
@@ -10,27 +10,32 @@ typedef struct ThreadNode
   int ltag, rtag;
 } ThreadNode, *ThreadTree;
 
-//中序线索化
+//中序线索化，在中序便利的过程中进行线索化
 ThreadNode *pre = NULL;
 void CreateInThread(ThreadTree T)
 {
   pre = NULL;
   if (T != NULL)
   {
+    //中序遍历二叉树
     InThread(T);
+    //处理遍历的最后一个结点
     if (pre->rchild == NULL)
       pre->rtag = 1;
   }
 }
 
+//访问时进行线索化
 void visit(ThreadNode *p)
 {
+  //如果当前结点的左指针为空，指向前一个结点
   if (p->lchild == NULL)
   {
     p->lchild = pre;
     p->ltag = 1;
   }
 
+  //  如果pre 结点的右指针为空，指向当前结点
   if (pre != NULL && pre->rchild == NULL)
   {
     pre->rchild = p;
@@ -42,7 +47,50 @@ void visit(ThreadNode *p)
 void InThread(ThreadTree T)
 {
   if (T != NULL)
+  {
     InThread(T->lchild);
-  visit(T);
-  InThread(T->rchild);
+    visit(T);
+    InThread(T->rchild);
+  }
+}
+
+/**
+ * 王道书上的中序线索化(同上种方法思路一样)
+ * 思路：pre指向前一个访问过得结点，p指向当前访问的结点
+ * 如果p的 左指针为空，将其指向pre
+ * 如果pre 的右指针为空，将其指向p
+ */
+void InThread2(ThreadTree p, ThreadTree &pre)
+{
+  if (p != NULL)
+  {
+    InThread2(p->lchild, pre);
+    if (p->lchild == NULL)
+    {
+      p->lchild = pre;
+      p->ltag = 1;
+    }
+
+    if (pre != NULL && pre->rchild == NULL)
+    {
+      pre->rchild = p;
+      pre->rtag = 1;
+    }
+
+    pre = p;
+    InThread2(p->rchild, pre);
+  }
+}
+
+void CreateInThread2(ThreadTree T)
+{
+  ThreadTree pre = NULL;
+  if (T != NULL)
+  {
+    //中序遍历二叉树
+    InThread2(T, pre);
+    //处理遍历的最后一个结点
+    if (pre->rchild == NULL)
+      pre->rtag = 1;
+  }
 }
