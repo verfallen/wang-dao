db7dce994127afd206859c735c84750fab121bf2
王晓丽 <wangxiaoli@troila.com>
2020-11-28 11:21:07 +0800

    折半查找(递归算法)

---
+++ chapter7/exercises/Binary_search.cpp
@@ -0,0 +1,14 @@
+//折半查找(递归算法)
+
+int BinSearchRec(int num[], int key, int low, int high)
+{
+  if (low > high)
+    return 0;
+  int mid = (low + high) / 2;
+  if (key > num[mid])
+    BinSearchRec(num, key, mid + 1, high);
+  else if (key < num[mid])
+    BinSearchRec(num, key, low, mid - 1);
+  else
+    return mid;
+}
