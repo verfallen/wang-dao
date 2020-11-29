37fa73e5ae5d32d75fe46b4f2a396a23559946d7
王晓丽 <wangxiaoli@troila.com>
2020-11-29 19:04:16 +0800

    简单选择排序

---
+++ chapter8/SelectSort.cpp
@@ -0,0 +1,22 @@
+void SelectSort(int A[], int n)
+{
+  for (int i = 0; i < n - 1; i++)
+  {
+    int min = i;
+    for (int j = i + 1; j < n; j++)
+    {
+      if (A[j] < A[min])
+        min = j;
+    }
+
+    if (min != i)
+      swap(A[i], A[min]);
+  }
+}
+
+void swap(int &a, int &b)
+{
+  int tmp = a;
+  a = b;
+  b = tmp;
+}
